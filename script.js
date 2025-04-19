20af21c1f79d70b3d69b8de0456b54b839b1f888
AndroMeda-py <abro2825@gmail.com>
2025-04-19 12:14:30 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('mouseover', () => {
-        link.style.color = '#FFD700';
-    });
-    link.addEventListener('mouseout', () => {
-        link.style.color = '#FF6347';
-    });
-});
