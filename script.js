cbb7d3ab25f56f91df0289c8cc1430e6e7f52ca0
bleyzbio <abro2825@gmail.com>
2025-04-19 23:25:48 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-let counter = 0;
-const counterDisplay = document.getElementById('counter');
-const clickBtn = document.getElementById('click-btn');
-
-clickBtn.addEventListener('click', () => {
-  counter++;
-  counterDisplay.textContent = counter;
-});
